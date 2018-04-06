ce625f6e501ab6ef81322c08a3141060e3e5845b
Linus <Linus@LinustekiMacBook-Air.local>
2018-04-07 02:04:22 +0800

    first

---
+++ config/default.js
@@ -0,0 +1,15 @@
+const config={
+    port:3000,
+
+    //database set
+    database:{
+        DATABASE:'tdm',
+        USERNAME:'root',
+        PASSWORD:'qwer1234',
+        PORT:'3306',
+        HOST:'localhost',
+        SOCKETPATH:'/Applications/MAMP/tmp/mysql/mysql.sock'
+    }
+}
+
+module.exports=config
